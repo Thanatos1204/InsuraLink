0a43f2d354fc7775633e4f07607bce3305ad79ce
Thanatos1204 <bhargavpandit01@gmail.com>
2023-12-17 06:04:47 +0530

    7th Commit

--- app/applyinsurance/page.js
+++ app/applyinsurance/page.js
@@ -2,11 +2,7 @@
 
 import React, { useState } from 'react'
 import './applyinsurance.css'
-<<<<<<< HEAD
 import Link from 'next/link'
-=======
-import toast, { Toaster } from 'react-hot-toast';
->>>>>>> f52a3f7a7c82c205f6de511281f5e96ff9629a8f
 
 
 function Applyinsurance() {
@@ -78,12 +74,7 @@ function Applyinsurance() {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
       </head>
       <header>
-<<<<<<< HEAD
             <Link class="logo" href="/">InsuraLink</Link>
-=======
-            
-            <a class="logo" href="/">Insure</a>
->>>>>>> f52a3f7a7c82c205f6de511281f5e96ff9629a8f
             <nav>
                 <ul class="nav__links">
                     <li><a href="#">Services</a></li>
