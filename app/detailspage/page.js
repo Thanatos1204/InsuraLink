4f3510673dea3d73c66faf4e076f5510c73c7410
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2023-12-27 20:25:05 +0530

    fetch details

--- app/detailspage/page.js
+++ app/detailspage/page.js
@@ -1,55 +1,54 @@
 'use client'
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import './detailspage.css'
-import { useState, useEffect } from 'react'
 import { UserAuth } from "../context/AuthContext";
 import axios from 'axios'
 
 function Detailspage() {
+ const [data, setData] = useState(null);
+ const { user } = UserAuth();
+ const [isLoading, setIsLoading] = useState(false); // State to handle loading
 
+ useEffect(() => {
+  const fetchData = async () => {
+    setIsLoading(true); // Set loading to true before starting the fetch
+    try {
+      const response = await axios.post('http://localhost:8080/getuserdetails', { useRef: 'CXq2HyZKvPNIQHbnyBBUr8kKa9E2'});
+      setData(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setIsLoading(false); // Set loading to false once the fetch is complete
+    }
+  };
 
+  fetchData();
+ }, []);
 
-const [data, setData] = useState(null);
-const { user } = UserAuth();
-
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post('http://localhost:8080/getuserdetails', { useRef: user.uid});
-        setData(response.data);
-        console.log(data)
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
- 
-    fetchData();
-  }, []);
-
- 
-  return (
-    <div className='detailspage'>
-        <Navbar/>
-        <div className='details'>
-            <p>Name : <span> Bhargav Pandit </span></p>
-            <p>Email : <span> bhargavpandit01@gmail.com </span></p>
-            <p>Gender : <span> Male </span></p>
-            <p>Contact : <span> 84250378054 </span></p>
-            <p>Address : <span> H Wing 702 Eternity Kanakia Spaces Thane (W) </span></p>
-            <p>Pincode : <span> 400604 </span></p>
-            <p>Nationality : <span> Indian </span></p>
-            <p>Martial Status : <span> Unmarried </span></p>
-            <p>Occupation : <span> Student </span></p>
-          <a>View Documents</a>            
-           
-        </div>
-      
+ return (
+  <div className='detailspage'>
+    <Navbar/>
+    <div className='details'>
+      {isLoading ? (
+        <p>Loading...</p> // Show loading text while fetching data
+      ) : (
+        <>
+          <p>Name : <span> {data?.firstname} {data?.lastname} </span></p>
+          <p>Email : <span> {data?.email} </span></p>
+          <p>Gender : <span> {data?.gender} </span></p>
+          <p>Contact : <span> {data?.contact} </span></p>
+          <p>Address : <span> {data?.address} </span></p>
+          <p>Pincode : <span> {data?.pincode} </span></p>
+          <p>Nationality : <span> {data?.nationality} </span></p>
+          <p>Martial Status : <span> {data?.Martial} </span></p>
+          <p>Occupation : <span> {data?.Occupation} </span></p>
+          <a href={data?.pdf} target='_blank'>View Documents</a>          
+        </>
+      )}
     </div>
-  )
+  </div>
+ )
 }
 
 export default Detailspage
